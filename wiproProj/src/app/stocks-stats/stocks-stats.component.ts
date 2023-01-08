c28854da5ef9e827cb7ce4d4657333bf0b5e238f
girivardhan <giri.nandyala.248@gmail.com>
2023-01-08 20:08:37 +0530

    values changed in stockstats

--- wiproProj/src/app/stocks-stats/stocks-stats.component.ts
+++ wiproProj/src/app/stocks-stats/stocks-stats.component.ts
@@ -8,19 +8,19 @@ export interface stats {
 
 const ELEMENT_DATA: stats[] = [
   {"stock": "NIFTY 50", "price": 18042.95,"score": -2.1},
-  {"stock": "TATA MOTORS", "price": 385, "score": 7770.0}, 
-  {"stock": "WIPRO", "price": 389, "score": 3960.0}, 
-  {"stock": "TECHM", "price": 1019, "score": -4360.0}, 
-  {"stock": "MARUTI", "price": 8419, "score": -5900.0},
-  {"stock": "DIVISLAB", "price": 3430, "score": -9874.0},
-  {"stock": "HDFCLIFE", "price": 597.9, "score": -11079.0}, 
-  {"stock": "TCS", "price": 3313, "score": -17470.0},
-  {"stock": "DRREDY", "price": 4241, "score": -18000.0}, 
-  {"stock": "ADANIENT", "price": 3822, "score": -25740.0}, 
-  {"stock": "UPL", "price": 715.1, "score": -25740.0}, 
-  {"stock": "SBILIFE", "price": 1260, "score": -25740.0}, 
-  {"stock": "BRITANNIA", "price": 4222.85, "score": -25740.0},
-  {"stock": "HCLTECH", "price": 1036.5, "score": -25740.0}
+  {"stock": "TATA MOTORS", "price": 385, "score": -3.0}, 
+  {"stock": "WIPRO", "price": 389, "score": 2.0}, 
+  {"stock": "TECHM", "price": 1019, "score": -1.3}, 
+  {"stock": "MARUTI", "price": 8419, "score": -0.5},
+  {"stock": "DIVISLAB", "price": 3430, "score": -1.0},
+  {"stock": "HDFCLIFE", "price": 597.9, "score": 1.3}, 
+  {"stock": "TCS", "price": 3313, "score": 2.3},
+  {"stock": "DRREDY", "price": 4241, "score": -1.5}, 
+  {"stock": "ADANIENT", "price": 3822, "score": 2.5}, 
+  {"stock": "UPL", "price": 715.1, "score": -0.75}, 
+  {"stock": "SBILIFE", "price": 1260, "score": -1.75}, 
+  {"stock": "BRITANNIA", "price": 4222.85, "score": 3.0},
+  {"stock": "HCLTECH", "price": 1036.5, "score": -1.0}
 ];
 
 
